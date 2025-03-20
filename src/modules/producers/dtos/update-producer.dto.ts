f6a4498e2c52ec7194fe966082abd28db8e26bb6
Gabriel Gomes de Amorim Silva <gabrielgomes@MacBook-Air-de-Gabriel.local>
2025-03-19 22:02:38 -0300

    feat: add swagger

--- src/modules/producers/dtos/update-producer.dto.ts
+++ src/modules/producers/dtos/update-producer.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, Matches } from 'class-validator';
 
 export class UpdateProducerDTO {
+  @ApiProperty({
+    description: 'The name of the producer',
+    example: 'John Doe',
+  })
   @IsOptional()
   @IsString({ message: 'Name must be a string' })
   name?: string;
 
+  @ApiProperty({
+    description: 'CPF or CNPJ of the producer',
+    example: '12345678901',
+  })
   @IsOptional()
   @Matches(/^\d{11}$|^\d{14}$/, {
     message: 'CPF must have 11 digits or CNPJ must have 14 digits',
